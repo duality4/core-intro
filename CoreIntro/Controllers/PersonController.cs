3aefc6803eefcbb2fbf17c1bdd16c6642ac073ae
TB <tb@example.com>
2019-05-10 15:15:16 +0200

    Add project files.

---
+++ CoreIntro/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIntro.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Person("Kalle", 10000);
+
+            return View(model);
+        }
+    }
+}
---
+++ CoreIntro/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreIntro.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public Person(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
