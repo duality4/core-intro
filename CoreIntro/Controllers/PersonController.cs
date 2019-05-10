1f2e23411f13990b7b5f7122942793cff8a9dbd1
TB <tb@example.com>
2019-05-10 15:24:04 +0200

    Add list /Person/AllPersons

--- CoreIntro/Controllers/PersonController.cs
+++ CoreIntro/Controllers/PersonController.cs
@@ -9,10 +9,20 @@ namespace CoreIntro.Controllers
 {
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string Name, int Salary)
         {
-            var model = new Person("Kalle", 10000);
+            var model = new Person(Name, Salary);
 
+            return View(model);
+        }
+        public IActionResult AllPersons(string Name, int Salary)
+        {
+            var model = new List<Person>
+            {
+                new Person("Kalle", 10000),
+                new Person("Lisa", 1000)
+            };
+            
             return View(model);
         }
     }
